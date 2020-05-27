e4196c55709613339825e77bc88c5235fc24f859
ABHISHEK SINGH <64076762+abk90133@users.noreply.github.com>
2020-05-27 18:15:53 +0530

    Add files via upload

---
+++ node file/index.js
@@ -0,0 +1,8 @@
+
+
+var supervillains = require("supervillains");
+
+var supervillains = supervillains.random();
+//this will generate a random superheroname in the console
+
+console.log(supervillains);
